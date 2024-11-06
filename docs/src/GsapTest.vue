2dc51c30a95803e8f929084a601ffd96f50c8f3b
alex <alex@temp.com>
2024-11-06 21:53:49 +0800

    GsapTest

--- docs/src/GsapTest.vue
+++ docs/src/GsapTest.vue
@@ -2,44 +2,47 @@
 <button @click="click" >运行</button>
 
 <div id="demo">
-  <div class="box green" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
-  <div class="box grey" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
-  <div class="box orange" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
-  <div class="box green" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
-  <div class="box grey" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
-  <div class="box orange" style="background-color:#989898;width: 50px;height: 50px;position: relative;border-radius: 6px;margin-top: 4px;display: inline-block"></div>
+  <div class="box green"></div>
+  <div class="box grey"></div>
+  <div class="box orange"></div>
+  <div class="box green"></div>
+  <div class="box grey"></div>
+  <div class="box orange"></div>
 </div>
 </template>
 
 <script setup>
 import gsap from 'gsap';
 function click() {
-    console.log("执行动画")
     const tl = gsap.timeline(); 
-    tl.to(".box", { duration: 0.5, x: 100 })
-    .to(".box", { duration: 0.5, backgroundColor: "#f38630" }, "+=0.5") 
-    .to(".box", { duration: 5, x: 0, rotation: -360 }, "+=0.5")
+    tl.to(".box", { duration: 3, x: 100, rotation: 360 })
+    .to(".box", { duration: 3, x: 0})
 }
 </script>
 
 <style>
-
 #demo {
   height: 500px;
   width: 500px;
   margin-top: 4px;  
-  background-color: #6fb936;
+  background-color: #ffffff;
 }
 .box {
   width: 50px;
   height: 50px;
   position: relative;
   border-radius: 6px;
-  margin-top: 4px;
+  margin: 5px;
+  border-top: #000000 2px;
   display: inline-block
 }
-
 .green{
   background-color: #6fb936;
+}
+.grey {
+  background-color:#989898;
+}
+.orange {
+  background-color:#ff7722;
 }
 </style>
