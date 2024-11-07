abdcc9fe897a983ac93493e3f5942bf6c25b5924
lintaipeng <2851795221@qq.com>
2024-11-07 14:10:21 +0800

    GsapTest.vue

--- docs/src/component/GsapTest.vue
+++ docs/src/component/GsapTest.vue
@@ -1,32 +1,35 @@
 <template>
-<button @click="click" >运行</button>
-
-<div id="demo">
-  <div class="box green"></div>
-  <div class="box grey"></div>
-  <div class="box orange"></div>
-  <div class="box green"></div>
-  <div class="box grey"></div>
-  <div class="box orange"></div>
-</div>
+  <div>
+    <button @click="click">运行</button>
+    <div id="demo">
+      <div class="box green"></div>
+      <div class="box grey"></div>
+      <div class="box orange"></div>
+      <div class="box green"></div>
+      <div class="box grey"></div>
+      <div class="box orange"></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import gsap from 'gsap';
 function click() {
-    const tl = gsap.timeline(); 
-    tl.to(".box", { duration: 3, x: 100, rotation: 360 })
+  const tl = gsap.timeline();
+  tl.to(".box", { duration: 3, x: 100, rotation: 360 })
     .to(".box", { duration: 3, x: 0, rotation: 0 })
 }
 </script>
 
 <style>
 #demo {
-  height: 500px;
-  width: 500px;
-  margin-top: 4px;  
+  margin-top: 4px;
+  margin-top: 4px;
+  background-color: #ffffff;
+  margin-top: 4px;
   background-color: #ffffff;
 }
+
 .box {
   width: 50px;
   height: 50px;
@@ -36,13 +39,16 @@ function click() {
   border-top: #000000 2px;
   display: inline-block
 }
-.green{
+
+.green {
   background-color: #6fb936;
 }
+
 .grey {
-  background-color:#989898;
+  background-color: #989898;
 }
+
 .orange {
-  background-color:#ff7722;
+  background-color: #ff7722;
 }
 </style>
