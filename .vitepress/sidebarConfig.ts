7650248aaa89b235211967b374805468d42754aa
alex <alex@temp.com>
2025-04-06 11:11:43 +0800

    设计模式目录

--- .vitepress/sidebarConfig.ts
+++ .vitepress/sidebarConfig.ts
@@ -2,7 +2,7 @@ import { SidebarConfig } from "./generate-sidebar"
 
 export let config: SidebarConfig = {
     pathMap: {
-        'notes': {name: '随手记',withIndex: true},
+        'notes': { name: '随手记', withIndex: true },
         'maven': 'Maven',
         'juc': 'Java 多线程与并发',
         'database': '数据库',
@@ -10,21 +10,25 @@ export let config: SidebarConfig = {
             name: 'Spring',
             withIndex: true,
             subItems: {
-                'spring-framework': {name: 'Spring Framework',collapsed: false},
-                'spring-boot': {name: 'Spring Boot',collapsed: false},
-                'spring-mvc': {name: 'Spring MVC',collapsed: false}
+                'spring-framework': { name: 'Spring Framework', collapsed: false },
+                'spring-boot': { name: 'Spring Boot', collapsed: false },
+                'spring-mvc': { name: 'Spring MVC', collapsed: false }
             }
         },
         'know_jvm': {
             name: '深入理解Java虚拟机',
             withIndex: true,
             subItems: {
-                'part-1': {name: '第一部分 走进Java', collapsed: false},
-                'part-2': {name: '第二部分 自动内存管理', collapsed: false},
-                'part-3': {name: '第三部分 虚拟机执行子系统', collapsed: false},
-                'part-4': {name: '第四部分 程序编译与代码优化', collapsed: false},
-                'part-5': {name: '第五部分 高效并发', collapsed: false}
+                'part-1': { name: '第一部分 走进Java', collapsed: false },
+                'part-2': { name: '第二部分 自动内存管理', collapsed: false },
+                'part-3': { name: '第三部分 虚拟机执行子系统', collapsed: false },
+                'part-4': { name: '第四部分 程序编译与代码优化', collapsed: false },
+                'part-5': { name: '第五部分 高效并发', collapsed: false }
             }
+        },
+        'design_pattern': {
+            name: '设计模式',
+            withIndex: false
         }
     },
     ignoredPath: [
